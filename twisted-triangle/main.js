b09f263abd9a420e81b760c9d702b6b49f1dd05b
Eric_Lian <git@superexercisebook.com>
2020-09-18 15:49:57 +0800

    让 WebGL 自动填充的扭曲三角形

--- twisted-triangle/main.js
+++ twisted-triangle/main.js
@@ -4,6 +4,8 @@ var center;
 
 var max_depth = 6;
 
+var points = [];
+var angle = 48 * 3.1415926535 / 180;
 var iii = 0;
 
 window.onload = function() {
@@ -17,12 +19,8 @@ window.onload = function() {
     var program = createProgram(gl, [vertexShader, fragmentShader]);
     gl.useProgram(program);
 
-    // set color
-    //var colorLocation = gl.getUniformLocation(program, "u_color");
-
     // set the resolution
     var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
-    //console.log(canvas.width, canvas.height);
     gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
 
     // Create a buffer
@@ -32,39 +30,19 @@ window.onload = function() {
     gl.enableVertexAttribArray(resolutionLocation);
     gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // 旋转因子
-    gl_rotation = gl.getUniformLocation(program, "multiplier");
-
-
+    // 创建点列表
     p1 = {x: -1 * 0.5, y: -1 * 0.5                  };
     p2 = {x:  1 * 0.5, y: -1 * 0.5                  };
     p3 = {x:  0 * 0.5, y: (Math.sqrt(3) - 1) * 0.5  };
     center = middle3(p1, p2, p3);
-    drawing(1, p1, p2, p3);
+    tessellation(1, p1, p2, p3);
 
+    console.log(points);
 
+    // 让 WebGL 将他们连起来
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
 
-
-    //setRotate(0 * 3.1415926535 / 180);
-//
-    //setTriangle(gl,
-    //    -1, -1, 1, -1, 0, Math.sqrt(3) - 1,
-    //);
-//
-    //gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-}
-
-function setTriangle(gl, p1, p2, p3){
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        p1.x, p1.y,
-        p2.x, p2.y,
-        p3.x, p3.y
-        ]), gl.STATIC_DRAW);
-}
-
-function setRotate(rotation){
-    gl.uniform2f(gl_rotation, Math.sin(rotation), Math.cos(rotation));
 }
 
 function middle2(p1, p2) {
@@ -79,44 +57,49 @@ function distance(p1, p2) {
     return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
 }
 
-function drawing(depth, p1, p2, p3) {
+function tessellation(depth, p1, p2, p3) {
     // console.log(depth);
 
     if (depth < max_depth) {
         depth++;
-        m1 = middle2(p1, p2);
-        m3 = middle2(p3, p1);
-        drawing(depth, p1, m1, m3);
 
         m1 = middle2(p1, p2);
-        m2 = middle2(p2, p3);
-        drawing(depth, m1, p2, m2);
-
-        m2 = middle2(p2, p3);
         m3 = middle2(p3, p1);
-        drawing(depth, m3, m2, p3);
+        tessellation(depth, p1, m1, m3);
 
         m1 = middle2(p1, p2);
         m2 = middle2(p2, p3);
+        tessellation(depth, m1, p2, m2);
+
+        m2 = middle2(p2, p3);
         m3 = middle2(p3, p1);
-        drawing(depth, m1, m2, m3);
+        tessellation(depth, m3, m2, p3);
 
-
+        m1 = middle2(p1, p2);
+        m2 = middle2(p2, p3);
+        m3 = middle2(p3, p1);
+        tessellation(depth, m1, m2, m3);
     } else {
-    
-        iii++;
-        console.log("A_" + iii + "=(" + p1.x + "," + p1.y + ")");
-        console.log("B_" + iii + "=(" + p2.x + "," + p2.y + ")");
-        console.log("C_" + iii + "=(" + p3.x + "," + p3.y + ")");
-        
-        M = middle3(p1, p2, p3);
-        M = distance(center, M) * 64;
-
-        setRotate(M * 3.1415926535 / 180);
-        setTriangle(gl, p1, p2, p3);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        p1 = twist(p1);
+        p2 = twist(p2);
+        p3 = twist(p3);
+        points.push(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1]);
     }
 
+}
 
+function twist(vector){
+    var x = vector.x,
+        y = vector.y,
+        d = Math.sqrt(x * x + y * y),
+        sinAngle = Math.sin(d * angle),
+        cosAngle = Math.cos(d * angle);
 
+        /*
+            x' = x cos(d0) - y sin(d0)
+            y' = x sin(d0) + y cos(d0)
+
+        */
+
+        return [x * cosAngle - y * sinAngle, x * sinAngle + y * cosAngle];
 }
